7f115dee0fff96acb28fd32b26baabdc18cc78ec
miken22 <mikenowicki022@gmail.com>
2015-01-12 08:37:04 -0800

    New project

---
+++ src/Board.java
@@ -0,0 +1,11 @@
+import ubco.ai.games.BoardGameModel;
+
+
+public class Board extends BoardGameModel {
+
+	public Board(int rows, int columns) {
+		super(rows, columns);
+		
+	}
+
+}
---
+++ src/Player.java
@@ -0,0 +1,45 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import ubco.ai.GameRoom;
+import ubco.ai.connection.ServerMessage;
+import ubco.ai.games.GameClient;
+import ubco.ai.games.GameMessage;
+import ubco.ai.games.GamePlayer;
+
+
+public class Player implements GamePlayer {
+
+	GameClient client;
+	
+	public Player(String userName, String password){
+		
+		client = new GameClient(userName, password, this);		
+		ArrayList<GameRoom> rooms = client.getRoomLists();
+		
+		for (GameRoom g : rooms){
+			
+		}
+	}
+	
+	@Override
+	public boolean handleMessage(String arg0) throws Exception {
+		
+		return false;
+	}
+
+	@Override
+	public boolean handleMessage(GameMessage arg0) throws Exception {
+		System.out.println("The server says: " + arg0);
+		return false;
+	}
+	
+	public void sendToServer(String msg, int roomID){
+		
+		String actionMsg = "Try this";
+
+		String message = ServerMessage.compileGameMessage(msg, roomID, actionMsg);
+		
+	}
+
+}
