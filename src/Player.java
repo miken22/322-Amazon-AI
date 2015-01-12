ed5b4fd6f24a2562a2a296b40fc54143bf91e163
miken22 <mikenowicki022@gmail.com>
2015-01-12 10:15:54 -0800

    Cleaning

--- src/Player.java
+++ src/Player.java
@@ -20,7 +20,6 @@ public class Player extends JFrame implements GamePlayer {
 		
 		client = new GameClient(userName, password, this);		
 		
-<<<<<<< HEAD
 		client.roomList = getRooms();
 		
 		client.getUserID();
@@ -37,25 +36,15 @@ public class Player extends JFrame implements GamePlayer {
 		}
 	}
 	
-	public ArrayList<GameRoom> getRooms(){
-		
-=======
-	}
 	
 	public ArrayList<GameRoom> getRooms(){
->>>>>>> 42c9945b9740b4515ee8a319a46f1349ad70ac78
+
 		ArrayList<GameRoom> rooms = client.getRoomLists();
 		
 		for(GameRoom g : rooms){
 			System.out.println(g.roomID);
-<<<<<<< HEAD
 			System.out.println(g.roomName);	
 		}
-
-=======
-			System.out.println(g.roomName);
-		}
->>>>>>> 42c9945b9740b4515ee8a319a46f1349ad70ac78
 		return rooms;
 	}
 	
